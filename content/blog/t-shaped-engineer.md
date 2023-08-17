785dccaec33e4e8624a36019dda58ed33c51fabc
Samip Pokharel <callmesamip@gmail.com>
2023-08-17 21:10:28 +1000

    Initialised Git Repository

---
+++ content/blog/t-shaped-engineer.md
@@ -0,0 +1,8 @@
+---
+external: false
+title: 'The T-Shaped Engineer'
+description: 'I am starting on a journey.'
+date: 2023-08-17
+---
+
+## What is a T-Shaped Engineer ?
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Samip Pokharel';
+export const SITE_DESCRIPTION = 'Welcome to my blog ! ';
+export const TWITTER_HANDLE = '';
+export const MY_NAME = 'Samip Pokharel';
+
+// setup in astro.config.mjs
+const BASE_URL = new URL(import.meta.env.SITE);
+export const SITE_URL = BASE_URL.origin;
